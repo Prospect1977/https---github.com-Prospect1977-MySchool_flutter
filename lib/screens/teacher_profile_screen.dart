e3d22bee791a7860e98502d872862a88819de68e
mohamed essm <langara5000@hotmail.com>
2023-03-01 17:03:06 +0200

    1-3-2023

---
+++ lib/screens/teacher_profile_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TeacherProfileScreen extends StatefulWidget {
+  const TeacherProfileScreen({Key key}) : super(key: key);
+
+  @override
+  State<TeacherProfileScreen> createState() => _TeacherProfileScreenState();
+}
+
+class _TeacherProfileScreenState extends State<TeacherProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
